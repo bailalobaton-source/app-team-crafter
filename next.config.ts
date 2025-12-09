7547f4e772f9078234ca0f782402afbf253deca6
Emerson Fernadez <escanorrosa123@gmail.com>
2025-12-09 16:37:22 -0500

    asaaaaaaaaaaaa

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       "end-point.team-crafter.com",
       "localhost",
       "www.paypalobjects.com",
-      "server-image-team-crafter-production.up.railway.app",
+      "files.team-crafter.com",
     ],
   },
 };
